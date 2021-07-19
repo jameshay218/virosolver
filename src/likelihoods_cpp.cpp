9036eb11f4bab1b0d86d66b423c2b120ed79821b
James Hay <jameshay218@gmail.com>
2021-07-19 16:17:00 -0400

    Added version of the Ct distribution prediction function for symptom based surveillance

--- src/likelihoods_cpp.cpp
+++ src/likelihoods_cpp.cpp
@@ -311,3 +311,109 @@ NumericVector pred_dist_cpp(NumericVector test_cts,
   return density;
 }
 
+
+// [[Rcpp::export]]
+NumericVector pred_dist_cpp_symptoms(NumericVector test_cts,
+                            int max_incu_period,
+                            int max_sampling_delay,
+                            double obs_time,
+                            NumericVector pars,
+                            NumericVector prob_infection,
+                            NumericVector sd_mod_vec){
+  
+  // Viral kinetics parameters
+  double lod = pars["LOD"];
+  double tshift = pars["tshift"];
+  double desired_mode = pars["desired_mode"];
+  double t_switch = pars["t_switch"];
+  double viral_peak = pars["viral_peak"];
+  double obs_sd = pars["obs_sd"];
+  double level_switch = pars["level_switch"];
+  double true_0 = pars["true_0"];
+  double yintercept = pars["intercept"];
+  double prob_detect = pars["prob_detect"];
+  
+  // Transformed viral kinetics parameters
+  double wane_rate = (viral_peak - level_switch)/t_switch;
+  double wane_rate2 = (level_switch - yintercept)/pars["wane_rate2"];
+  double growth_rate = (viral_peak - true_0)/desired_mode;
+  double t_switch1 = t_switch + desired_mode + tshift;
+  
+  // Incubation period parameters
+  double incu_par1 = pars["incu_par1"];
+  double incu_par2 = pars["incu_par2"];
+  
+  // Sampling delay parameters
+  double sampling_par1 = pars["sampling_par1"];
+  double sampling_par2 = pars["sampling_par2"];
+  
+  int age_max = max_incu_period + max_sampling_delay;
+  
+  NumericVector vl_ages(age_max+1);
+  NumericVector renormalizes(age_max+1);
+  double prob_undetectable=0;
+  
+  // Solve model for expected viral load on each day of infection
+  for(int a = 0; a < vl_ages.size(); ++a){
+    vl_ages[a] = viral_load_func_single_cpp(tshift,desired_mode, t_switch,viral_peak,
+                                            obs_sd*sd_mod_vec[a],level_switch,true_0, yintercept,
+                                            lod,wane_rate, wane_rate2,growth_rate,
+                                            a,false);
+    renormalizes[a] = pgumbel_scale(yintercept, vl_ages[a],obs_sd*sd_mod_vec[a]);
+  }
+  
+  // Solve model for proportion detectable (including uninfected) on each day of infection
+  NumericVector prob_detectable_dat(age_max+1);
+  for(int a = 0; a < prob_detectable_dat.size(); ++a){
+    prob_detectable_dat[a] = prop_detectable_cpp(a, vl_ages[a],obs_sd*sd_mod_vec[a],
+                                                 yintercept,t_switch1, prob_detect);
+    prob_undetectable += prob_detectable_dat[a]*prob_infection[obs_time-a];
+    
+    // If no one is detectable of a certain age since infection, then we will be dividing
+    // 0 by renormalizes[a], so set to 1 to prevent error
+    if(prob_detectable_dat[a] == 0) {
+      renormalizes[a] = 1;
+    }
+  }
+  prob_undetectable = 1 - prob_undetectable;
+  
+  // Find probability of each discrete incubation period
+  NumericVector prob_incu_period(max_incu_period+1);
+  for(int o = 0; o < prob_incu_period.size(); ++o){
+    prob_incu_period[o] = (R::plnorm(o+1, incu_par1, incu_par2, true, false) - R::plnorm(o, incu_par1, incu_par2, true, false))/
+      R::plnorm(max_incu_period, incu_par1, incu_par2, true, false);
+  }
+  
+  // Find probability of each discrete sampling delay
+  NumericVector prob_sampling_delay(max_sampling_delay+1);
+  for(int d = 0; d < prob_sampling_delay.size(); ++d){
+    prob_sampling_delay[d] = (R::pgamma(d+1, sampling_par1, sampling_par2, true, false) - R::pgamma(d, sampling_par1, sampling_par2, true, false))/
+      R::pgamma(max_sampling_delay, sampling_par1, sampling_par2, true, false);
+  }
+  
+  NumericVector density(test_cts.size());
+  // For each observation
+  int a;
+  for(int i = 0; i < test_cts.size(); ++i){
+    for(int d = 0; d < prob_sampling_delay.size(); ++d){
+      for(int o = 0; o < prob_incu_period.size(); ++o){
+        a = d + o; // Time since infection is days since onset + sampling delay
+        if(test_cts[i] >= yintercept){
+          density[i] = prob_undetectable;
+        } else {
+          density[i] += ((
+            pgumbel_jh(test_cts[i]+1, vl_ages[a], obs_sd*sd_mod_vec[a])-
+              pgumbel_jh(test_cts[i], vl_ages[a], obs_sd*sd_mod_vec[a])
+          )*
+            prob_sampling_delay[d]* // Probability of having this sampling delay
+            prob_incu_period[o]* // Probability of having this incubation period
+            prob_infection[obs_time-a]*
+            prob_detectable_dat[a])/
+              renormalizes[a]  ;
+        }
+      }
+    }
+  }
+  return density;
+}
+
