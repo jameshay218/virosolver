d9d4ba749c3beb0af0dbcd47180c75e0292c74e4
kcharniga <k.charniga17@imperial.ac.uk>
2021-08-03 10:55:59 -0400

    Added data for vignettes and documented the data files

--- R/data.R
+++ R/data.R
@@ -27,7 +27,6 @@
 #' For this parameter table, we have entries for the GP prior parameters (the latent states for each day "prob", the hyperparameters for the exponential kernel function, rho and nu), as well as
 #' entries for the viral kinetics/Ct model (peak Ct value, waning duration, time to peak, variation about the model Ct curve etc).
 #' @docType data
-#' @docType data
 #' @usage data(example_gp_partab)
 #' @format A data frame with 384 rows and 8 variables:
 #' \describe{
@@ -48,7 +47,6 @@
 #'
 #' Observed Ct values simulated from an SEIR model, with 2000 individuals sampled at random every 14 days between days 55 and 153 of the simulation. Each entry is one anonymised individual sample.
 #' @docType data
-#' @docType data
 #' @usage data(example_ct_data)
 #' @format A data frame with 15984 rows and 2 variables:
 #' \describe{
@@ -62,7 +60,6 @@
 #' 
 #' The true SEIR incidence curve used in the case study simulation
 #' @docType data
-#' @docType data
 #' @usage data(example_seir_incidence)
 #' @format A tible with 251 rows and 2 columns
 #' \describe{
@@ -71,3 +68,27 @@
 #' }
 #' @family example_data
 "example_seir_incidence"
+
+#' Simulated SEIR incidence for vignettes
+#' 
+#' This is the raw incidence from the simulate_seir_process function. It is created in vignette 1 and used in vignette 2. 
+#' @docType data
+#' @usage data(vignette_data1)
+#' @format Numeric vector with length 151
+#' @family example_data
+"vignette_data1"
+
+#' Simulated Ct values for vignettes
+#' 
+#' These are the simulated Ct values which are created in vignette 1 and used in vignette 2. 
+#' @docType data
+#' @usage data(vignette_data2)
+#' @format A data frame with 80000 rows and and 4 columns
+#' \describe{
+#'     \item{sampled_time}{Time of sample collection in days}
+#'     \item{Ct}{Ct values}
+#'     \item{date}{calendar date}
+#'     \item{Ct_round}{rounded Ct values}
+#' }
+#' @family example_data
+"vignette_data2"
